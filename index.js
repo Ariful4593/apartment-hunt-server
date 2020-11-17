cfc7ca79003a267274c691d9dd4a1392ec885ec6
Ariful4593 <arifkhagoria2015@gmail.com>
2020-11-17 17:43:37 +0600

    Code Updated

--- index.js
+++ index.js
@@ -1,94 +1,113 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
-const { ObjectId, ObjectID } = require('mongodb');
-require('dotenv').config();
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+require('dotenv').config()
+const { ObjectId, ObjectID } = require('mongodb')
 
-app.use(bodyParser.json());
-app.use(cors());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.extbg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static('doctors'))
 app.use(fileUpload())
-app.get('/', (req, res) => {
-    res.send("Hello Apartment Hunt")
-})
 
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xsirj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const bookinglist = client.db("apartmentHunt").collection("bookinglist");
-    const addRentHouse = client.db("apartmentHunt").collection("addrenthouse");
-    const admin = client.db("apartmentHunt").collection("admin");
+    const roomCollection = client.db("apartmentHunt").collection("addroom")
+    const bookingListCollection = client.db("apartmentHunt").collection("addbooking")
 
-    console.log('Database connected')
-    app.post('/addBooking', (req, res) => {
-        const data = req.body;
-        bookinglist.insertOne(data)
-            .then((result) => {
-                res.send(result.insertedCount > 0)
-            })
-
+    app.get('/', (req, res) => {
+        res.send('Apartment hunt server is running...')
     })
 
-    app.get('/getBookingList', (req, res) => {
-        bookinglist.find({})
-            .toArray((err, document) => {
-                res.send(document)
-            })
-    })
-
+    // @desc    Add all rent house
+    // @route   POST /addRentHouse
     app.post('/addRentHouse', (req, res) => {
-        const file = req.files.file;
-        const title = req.body.title;
-        const price = req.body.price;
-        const location = req.body.location;
-        const bedroom = req.body.bedroom;
-        const bathroom = req.body.bath;
-
-
-        const newImg = file.data;
-        const encImg = newImg.toString('base64');
+        const file = req.files.file
+        const title = req.body.title
+        const location = req.body.location
+        const bathroom = req.body.bathroom
+        const bedroom = req.body.bedroom
+        const price = req.body.price
+        console.log(file, title, location, bathroom, bedroom, price)
+        const newImg = file.data
+        const encImg = newImg.toString('base64')
 
         var image = {
             contentType: req.files.file.mimetype,
             size: req.files.file.size,
-            img: Buffer.from(encImg, 'base64'),
-        }
+            img: Buffer.from(encImg, 'base64')
+        };
 
-        addRentHouse.insertOne({ title, price, location, bedroom, bathroom, image })
+        roomCollection.insertOne({ title, location, bathroom, bedroom, price, image })
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    // @desc    get rent house data
+    // @route   GET /getRentHouseData
+    app.get('/getRentHouseData', (req, res) => {
+        roomCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+    // @desc    Get dynamic data by id
+    // @route   GET /room/:id
+    app.get('/room/:id', (req, res) => {
+        const id = req.params.id
+        roomCollection.find({ _id: ObjectId(id) })
+            .toArray((err, documents) => {
+            res.send(documents)
+        })
+    })
+
+    // @desc    Add all booking
+    // @route   POST /addBooking
+    app.post('/addBooking', (req, res) => {
+        const data = req.body
+        console.log(data)
+        bookingListCollection.insertOne(data)
             .then((result) => {
                 res.send(result.insertedCount > 0)
             })
     })
 
-    app.get('/getRentHouseData', (req, res) => {
-        addRentHouse.find({})
-            .toArray((err, document) => {
-                res.send(document)
-            })
-    })
-
-    app.post('/getClientPlacedRent', (req, res) => {
-        const email = req.body.email;
-        bookinglist.find({ email: email })
+    // @desc    Get all booking data
+    // @route   GET /getRentHouseData
+    app.get('/getBooking', (req, res) => {
+        const email = req.query.email
+        console.log(email)
+        bookingListCollection.find({email: email})
             .toArray((err, documents) => {
-                res.send(documents);
+                res.send(documents)
             })
     })
-    app.post('/isAdmin', (req, res) =>{
-        const email = req.body.email;
-        admin.find({email: email})
-        .toArray((err, admins) =>{
-            res.send(admins.length > 0)
-        })
+
+    // @desc    get rent house data
+    // @route   GET /myRent
+    app.get('/myRent', (req, res) => {
+        roomCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
     })
 
-    app.patch('/update', (req, res) => {
-
-        bookinglist.updateOne(
-            { _id: ObjectId(req.body.id) },
+        // @desc    Update booking status
+        // @route   patch /update
+        app.patch('/update', (req, res) => {
+        bookingListCollection.updateOne(
+            { _id: ObjectId(req.body._id) },
             {
                 $set: { 'status': req.body.status }
             }
@@ -97,9 +116,9 @@ client.connect(err => {
                 console.log(result)
             })
     })
-});
+
+    
+})
 
 
-
-const port = 4000;
-app.listen(process.env.PORT || port)
+app.listen(process.env.PORT || 5000, () => console.log('listening on port 5000'))
